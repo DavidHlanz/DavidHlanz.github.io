fd549afc62109d862c58eb47302b8d3adba6f18f
David <davidhlanz@gmail.com>
2025-03-04 08:44:14 +0100

    Grandes Cambios (Nuevos apartados de noticias y testimonios y modo oscuro con boton) Añadir script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+
+    const toggleButton = document.getElementById("dark-mode-toggle");
+    const body = document.body;
+
+    // Comprobar la preferencia guardada
+    if (localStorage.getItem("darkMode") === "enabled") {
+        body.classList.add("dark-mode");
+    }
+
+    toggleButton.addEventListener("click", () => {
+        body.classList.toggle("dark-mode");
+
+        // Guardar la preferencia en localStorage
+        if (body.classList.contains("dark-mode")) {
+            localStorage.setItem("darkMode", "enabled");
+        } else {
+            localStorage.setItem("darkMode", "disabled");
+        }
+    });
+
